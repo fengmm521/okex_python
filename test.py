2c012350d632ac547a969d13af16e1a54491848d
woodcol <fengmm521@gmail.com>
2018-08-19 12:26:33 +0800

    add test

--- test.py
+++ test.py
@@ -3,11 +3,12 @@
 # encoding: utf-8
 #客户端调用，用于查看API返回结果
 
-
+import time
 if __name__ == '__main__':
     # a = ['a','b','c']
     # x = ','.join(a)
-    a = ' 345 abc  jjj   '
-    x = ' '.join(a.split())
-    print x
-   
+    # a = ' 345 abc  jjj   '
+    # x = ' '.join(a.split())
+    # print x
+    a = str(int(time.time() - 100*60*60)*1000)
+    print(a)
